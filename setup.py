3e4e44968412acb2ada2279b6fb4108eb66b02b2
Chris Teplovs <chris.teplovs@gmail.com>
2014-08-15 00:23:20 -0400

    Remove dev indentifier; crank Development Status; updated URL to point to project page

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='kf5py',
     py_modules = ['kf5py'],
-    version='0.1.dev5',
+    version='0.1.0',
     author='Chris Teplovs',
     author_email='dr.chris@problemshift.com',
-    url='https://github.com/problemshift/kf5py',
+    url='http://problemshift.github.io/kf5py/',
     license='LICENSE.txt',
     description='Python-based utilities for KF5.',
     install_requires=[
@@ -16,7 +16,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
-        "Development Status :: 1 - Planning",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
