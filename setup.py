b464caf23f45232c44ea30882b41f4f5c62f9947
Chris Teplovs <chris.teplovs@gmail.com>
2014-12-16 16:06:56 +0100

    More typos

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='kf5py',
     py_modules = ['kf5py'],
-    version='0.1.2',
+    version='0.1.3',
     author='Chris Teplovs',
     author_email='dr.chris@problemshift.com',
     url='http://problemshift.github.io/kf5py/',
